67cc396ba9cfdbab53e78b8751e1a035e96c2489
Mehmet Dogan <info@intelligentworks.ch>
2017-09-17 19:45:56 +0200

    find port

--- js/module.js
+++ js/module.js
@@ -19,50 +19,16 @@ function analyseUrl(pUrl) {
     }
 
     // I extract to schema from the URL ... 
-    analyseResult.schema = pUrl.slice(0, pUrl.indexOf("://"));
+    analyseResult.schema = findSchema(pUrl);
+    analyseResult.host = findHost(pUrl);
+    analyseResult.port = fincPort(pUrl);
+    
 
-    /* I exract to host from URL ... I want to use slice method ... so firstly I have to find the start position ... For slice method i will find the start position with pUrl.indexOf method ... below you can see how I found .... pay attention the 7 .... there are two ":" symbolls and i dont want to start with first one , and say the method find the last one.... but there is another condition about this situation that if the URL includes "#" symbolls.  
-     */
-    if (pUrl.includes(":", 7) == true) {
-        analyseResult.host = pUrl.slice(pUrl.indexOf("//") + 2, pUrl.indexOf(":", 7));
-
-    } else if (pUrl.includes("/", 10)) {
-        analyseResult.host = pUrl.slice(pUrl.indexOf("//") + 2, pUrl.indexOf("/", 10));
-    } else if (pUrl.includes("?")) {
-        analyseResult.host = pUrl.slice(pUrl.indexOf("//") + 2, pUrl.indexOf("?"));
-    } else if (pUrl.includes("#")) {
-        analyseResult.host = pUrl.slice(pUrl.indexOf("//") + 2, pUrl.indexOf("#"));
-    } else {
-        analyseResult.host = pUrl.slice(pUrl.indexOf("//") + 2);
-    }
-
-    /*       
-    port
-    */
-    if (pUrl.includes(":", 10)) {
-        if (pUrl.includes("/", 10)) {
-            analyseResult.port = pUrl.slice(pUrl.indexOf(":", 10) + 1, pUrl.indexOf("/", 10));
-        } else if (pUrl.includes("?")) {
-            analyseResult.port = pUrl.slice(pUrl.indexOf(":", 10) + 1, pUrl.indexOf("?"));
-        } else if (pUrl.includes("#")) {
-            analyseResult.port = pUrl.slice(pUrl.indexOf(":", 10) + 1, pUrl.indexOf("#"));
-        } else {
-            analyseResult.port = pUrl.slice(pUrl.indexOf(":", 10) + 1);
-        }
-    }
 
     /*
     path
     */
-    if (pUrl.includes("/", 10)) {
-        if (pUrl.includes("?")) {
-            analyseResult.path = pUrl.slice(pUrl.indexOf("/", 10) + 1, pUrl.indexOf("?"));
-        } else if (pUrl.includes("#")) {
-            analyseResult.port = pUrl.slice(pUrl.indexOf("/", 10) + 1, pUrl.indexOf("#"));
-        } else {
-            analyseResult.port = pUrl.slice(pUrl.indexOf("/", 10) + 1);
-        }
-    }
+
 
 
     /* I want to find query ... all gueries start with "?" symboll... So I will start with If and i will write as condition inside "if" it is true that there is "?" inside pUrl.... and then if there is one, and then i will use "if" and 'else' ... first 'if' says that query ends with "#" and else says that query doesnt end with anything.
@@ -83,9 +49,13 @@ function analyseUrl(pUrl) {
 }
 
 
-function findHost(pUrl){
+function findSchema(pUrl) {
+    return pUrl.slice(0, pUrl.indexOf("://"));
+}
+
+function findHost(pUrl) {
     let host = null;
-    
+
     if (pUrl.includes(":", 7) == true) {
         host = pUrl.slice(pUrl.indexOf("//") + 2, pUrl.indexOf(":", 7));
     } else if (pUrl.includes("/", 10)) {
@@ -97,6 +67,24 @@ function findHost(pUrl){
     } else {
         host = pUrl.slice(pUrl.indexOf("//") + 2);
     }
-    
-    return host
+
+    return host;
 }
+
+function findPort(pUrl) {
+    let port = null;
+    if (pUrl.includes(":", 10)) {
+        if (pUrl.includes("/", 10)) {
+            port = pUrl.slice(pUrl.indexOf(":", 10) + 1, pUrl.indexOf("/", 10));
+        } else if (pUrl.includes("?")) {
+            port = pUrl.slice(pUrl.indexOf(":", 10) + 1, pUrl.indexOf("?"));
+        } else if (pUrl.includes("#")) {
+            port = pUrl.slice(pUrl.indexOf(":", 10) + 1, pUrl.indexOf("#"));
+        } else {
+            port = pUrl.slice(pUrl.indexOf(":", 10) + 1);
+        }
+    }
+    
+    return port;
+}
+
