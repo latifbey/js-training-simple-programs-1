9afc13cd7e32f91c64a541f6faeee66903e5f9c9
Mehmet Dogan <info@intelligentworks.ch>
2017-09-18 22:54:09 +0200

    other alternatives are aso listed

--- js/module_super_solution.js
+++ js/module_super_solution.js
@@ -6,17 +6,23 @@
 "use strict";
 
 /**
- * A URL PARSER
+ * A URL PARSER by RegExp
  * This function analyses an url.
  * Please refer to the "https://de.wikipedia.org/wiki/Uniform_Resource_Locator"
  */
 function analyseUrl(pUrl) {
-    
     //let regExp = new RegExp();
-    
     aaaa="http://www.somesite.se/blah/sdgsdgsdgs";
     //aaaa="http://somesite.se/blah/sese";
         var matches = aaaa.match(/:\/\/(?:www\.)?(.[^/]+)(.*)/);
         alert(matches[1]);
         alert(matches[2]);
 }
+
+// A1
+// <a href="" />
+
+// A2
+// const url = new URL('../cats', 'http://www.example.com/dogs');
+// console.log(url.hostname); // "www.example.com"
+// console.log(url.pathname); // "/cats"
