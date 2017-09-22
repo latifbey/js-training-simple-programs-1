e36a0a21f445ecf8d5eacaffefcfd0d4e4d95b85
Mehmet Dogan <info@intelligentworks.ch>
2017-09-22 23:58:44 +0200

    closure

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@
 "use strict";
 
 
-var urlObject = analyseUrl('https://www.google.ch/search?q=url');
-console.log("HOST:", urlObject.host)
-console.log("PATH:", urlObject.path)
-console.log("QUERY:", urlObject.query)
+{
+    var urlObject = analyseUrl('https://www.google.ch/search?q=url');
+    console.log("HOST:", urlObject.host)
+    console.log("PATH:", urlObject.path)
+    console.log("QUERY:", urlObject.query)
+}
