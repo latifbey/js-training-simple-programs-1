8fb20183ccd09c8e834c306f6a6abf3d21952a7a
Mehmet Dogan <info@intelligentworks.ch>
2017-09-23 08:40:08 +0200

    cutom object

---
+++ js/customObject/index.js
@@ -0,0 +1,13 @@
+/**
+ * There are many ways to create a custom object with own variable and methods.
+ *
+ * 1) Function constructor
+ * 2) Object.create()
+ * 3) Factory function
+ * 4) Classes
+ *
+ */
+
+function Person(){
+    
+}
--- js/main.js
+++ js/main.js
@@ -5,15 +5,28 @@
 
 "use strict";
 
-
-{
+// Every function has its method
+(() => {
     var urlObject = analyseUrl('https://www.google.ch/search?q=url');
     console.log("HOST:", urlObject.host)
     console.log("PATH:", urlObject.path)
     console.log("QUERY:", urlObject.query)
-}
+})();
 
 
-{
-    
-}
+(function() => {
+    let palindroms = findPalindroms("merhaba dostlar ses verin de karpuz kesek");
+    console.log(palindroms);
+})();
+
+
+(function(){
+    let gcd = findGcd(9, 27);
+    console.log(9);
+})();
+
+
+(() => {
+    let gcd = findGcd(9, 27);
+    console.log(9);
+})();
